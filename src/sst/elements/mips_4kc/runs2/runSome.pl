74c9c2d6f518d24d72a7dcd341179c7498f82fa7
Arun Rodrigues <afrodri@sandia.gov>
2020-09-17 13:50:31 -0600

    updated fault cross sections

--- src/sst/elements/mips_4kc/runs2/runSome.pl
+++ src/sst/elements/mips_4kc/runs2/runSome.pl
@@ -8,19 +8,25 @@
 %probabilities = (    
     "0x40D" => 64, #ALU	64
     #"" => , #CONTROL	52
-    "0x200I" => 8, #INST_ADDR	8
-    "0x400D" => 32, #INST_TYPE - data	32
-    "0x400C" => 5, #INST_TYPE - control	5
-    "0x4D" => 65, #MDU - data	65
-    "0x4C" => 14, #MDU - control	14
-    "0x10D" => 32, #MEM_POST -data	32
-    "0x10C" => 3, #MEM_POST - control	3
-    "0x8D" => 59, #MEM_PRE_ADDR -data	59
-    "0x1000D" => 32, #MEM_PRE_DATA -data	32
+    "0x200I" => 40, #INST_ADDR	data
+    "0x200C" => 16, #INST_ADDR	control
+    "0x400D" => 32, #INST_TYPE - data	
+    "0x400C" => 5, #INST_TYPE - control	
+    "0x4D" => 233, #MDU - data	
+    "0x4C" => 122, #MDU - control	
+    "0x10D" => 160, #MEM_POST -data	
+    "0x10C" => 8, #MEM_POST - control	
+    "0x8D" => 203, #MEM_PRE_ADDR -data
+    "0x8C" => 48, #MEM_PRE_ADDR -control
+    "0x8B" => 16, #MEM_PRE_ADDR -byte	
+    "0x1000D" => 256, #MEM_PRE_DATA -data	
+    "0x1000C" => 8, #MEM_PRE_DATA -control	
+    "0x1000B" => 23, #MEM_PRE_DATA -byte	
     "0x1D" => 992, #RF data	992
-    "0x800D" => 5, #RF control (WB_ADDR)	5
-    "0x80D" => 32, #MEM_bp_val -data	32
-    "0x20D" => 32 # WB 32
+    "0x800D" => 5, #RF control (WB_ADDR)	
+    "0x80D" => 32, #MEM_bp_val -data	
+    "0x20D" => 32, # WB
+    "0x2000D" => 64 #PC
     );
 $maxChance = 0;
 
@@ -52,10 +58,15 @@ sub getFault() {
     #determine which bits to flip
     my $type = chop($f);
     if ($type eq 'I') {
-        # pick one bit in 0b1111111100
-        my $roll2 = int(rand(8));
+        # try to get a 'valid' instruction which is still the wrong
+        # instruction. Pick one bit in 0b0000111100
+        my $roll2 = int(rand(4));
         my $b = 1 << ($roll2 + 2);
         #printf("I $roll2 %x\n", $b);
+    } elsif ($type eq 'B') {
+        # pick one byte
+        my $roll2 = int(rand(4));
+        my $b = 0xff << ($roll2 * 4);
     } elsif ($type eq 'D') {
         $b = 0;
     } elsif ($type eq 'C') {
@@ -83,7 +94,12 @@ while ($i < 100) {
         printf("$str\n");
     } else {
         system("echo \"---------------------$f-$b-$e\" 1>>${file} 2>&1");
-        system("${str} 1>>${file} 2>&1");
+        $ret = system("${str} 1>>${file} 2>&1");
+        if ($ret != 0) {
+            # got an error
+            printf("Err %d\n", $ret);
+            system("echo \"UNRECOVERABLE ERROR: ".$ret."\" 1>>${file} 2>&1");
+        }
     }
     $i++;
 }
